dca2a5206eb487a49d0361c2ce94b2615c908863
Samuel <samuel@ruegger.me>
2019-03-18 16:06:49 +0100

    Bugfix Gravityforms

--- wp-content/themes/handuffshaerz-wp-theme/inc/gravityforms.php
+++ wp-content/themes/handuffshaerz-wp-theme/inc/gravityforms.php
@@ -11,3 +11,11 @@ function add_custom_css_classes( $button, $form ) {
 	return $dom->saveHtml( $input );
 }
 add_filter( 'gform_submit_button', 'add_custom_css_classes', 10, 2 );
+
+/***************************************
+ * PLZ vor Ort im Adressfeld anzeigens
+ ***************************************/
+function address_format( $format ) {
+	return 'zip_before_city';
+}
+add_filter( 'gform_address_display_format', 'address_format' );
